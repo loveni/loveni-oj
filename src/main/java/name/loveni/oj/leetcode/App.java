5d0a4f8479e2ef3ec79fb0c614c1480321cf2e47
Xiangyu Hu <loveni@loveni.name>
2014-03-16 23:13:22 +0800

    init repo

---
+++ src/main/java/name/loveni/oj/leetcode/App.java
@@ -0,0 +1,13 @@
+package name.loveni.oj.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
